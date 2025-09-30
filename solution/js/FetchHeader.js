2095887954495ebf7bf3eb096863f406b999d4b7
Mads-Paaske <madsp24@student.sdu.dk>
2025-09-30 19:46:40 +0200

    Created a placeholder footer and added it to each page

--- solution/js/FetchHeader.js
+++ solution/js/FetchHeader.js
@@ -3,3 +3,9 @@ fetch ("Header.html")
     .then(data => {
         document.getElementById("header-placeholder").innerHTML = data;
     });
+
+fetch ("Footer.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("footer-placeholder").innerHTML = data;
+    });
